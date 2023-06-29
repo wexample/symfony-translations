aef782062b26264824098b75095b7ed2afd8d22a
weeger <romain.weeger@gmail.com>
2023-06-29 11:42:08 +0200

    Split extension

--- src/Twig/TranslationExtension.php
+++ src/Twig/TranslationExtension.php
@@ -3,28 +3,14 @@
 namespace Wexample\SymfonyTranslations\Twig;
 
 use Twig\TwigFunction;
-use Wexample\SymfonyDesignSystem\Service\LocaleService;
 use Wexample\SymfonyHelpers\Twig\AbstractExtension;
 use Wexample\SymfonyTranslations\Translation\Translator;
 
 class TranslationExtension extends AbstractExtension
 {
-    public function __construct(
-        public LocaleService $localeService,
-        public Translator $translator,
-    ) {
-    }
-
     public function getFunctions(): array
     {
         return [
-            new TwigFunction(
-                'trans_js',
-                [
-                    $this,
-                    'transJs',
-                ]
-            ),
             new TwigFunction(
                 'translation_build_domain_from_path',
                 [
@@ -40,14 +26,5 @@ class TranslationExtension extends AbstractExtension
         return Translator::buildDomainFromPath(
             $path
         );
-    }
-
-    /**
-     * Make translation available for javascript.
-     */
-    public function transJs(
-        string|array $keys
-    ): void {
-        $this->localeService->transJs($keys);
     }
 }
