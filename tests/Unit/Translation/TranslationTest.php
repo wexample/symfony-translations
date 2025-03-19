cf92f2d6ef1baa62d2d17c3784835c5dd5f271a4
weeger <romain.weeger@gmail.com>
2025-03-19 19:19:29 +0100

    Test domains stack

--- tests/Unit/Translation/TranslationTest.php
+++ tests/Unit/Translation/TranslationTest.php
@@ -158,4 +158,39 @@ class TranslationTest extends AbstractApplicationTestCase
             'Translation '.$domain.'::'.$key.' is translated as "'.$expectedValue.'"'
         );
     }
+
+    /**
+     * Test domain stack management
+     */
+    public function testDomainStack()
+    {
+        /** @var Translator $translator */
+        $translator = $this->translator;
+        
+        // Set up test domains
+        $translator->setDomain('context', '@test.domain.one');
+        $translator->setDomain('page', '@test.domain.two');
+        
+        // Test getting domains from stack
+        $this->assertEquals('@test.domain.one', $translator->getDomain('context'));
+        $this->assertEquals('@test.domain.two', $translator->getDomain('page'));
+        
+        // Test domain resolution with resolveDomain
+        $this->assertEquals('context', $translator->resolveDomain('context'));
+        $this->assertEquals('@test.domain.one', $translator->resolveDomain('@context'));
+        
+        // Test setting domain from path
+        $path = 'test/path/to/file.html.twig';
+        $domainFromPath = $translator->setDomainFromPath('path_domain', $path);
+        
+        // Verify the domain was set correctly
+        $this->assertNotNull($translator->getDomain('path_domain'));
+        $this->assertEquals($domainFromPath, $translator->getDomain('path_domain'));
+        
+        // Test domain reversion
+        $translator->setDomain('temp_domain', '@test.domain.three');
+        $this->assertEquals('@test.domain.three', $translator->getDomain('temp_domain'));
+        $translator->revertDomain('temp_domain');
+        $this->assertNull($translator->getDomain('temp_domain'));
+    }
 }
