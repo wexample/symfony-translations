19a7e629b58dbb950d555b305b9400fab0e56204
weeger <romain.weeger@gmail.com>
2025-03-24 12:24:35 +0100

    Tests works

--- src/Tests/AbstractTranslationTest.php
+++ src/Tests/AbstractTranslationTest.php
@@ -5,12 +5,14 @@ namespace Wexample\SymfonyTranslations\Tests;
 use Symfony\Bundle\FrameworkBundle\Translation\Translator as SymfonyTranslator;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Translation\MessageCatalogueInterface;
 use Wexample\SymfonyTesting\Tests\AbstractApplicationTestCase;
 use Wexample\SymfonyTranslations\Translation\Translator;
 
 abstract class AbstractTranslationTest extends AbstractApplicationTestCase
 {
     protected ?object $translator = null;
+    protected SymfonyTranslator $symTranslator;
 
     protected function setUp(): void
     {
@@ -42,6 +44,43 @@ abstract class AbstractTranslationTest extends AbstractApplicationTestCase
 
         // Create the translator instance
         $this->translator = new Translator($symTranslator, $kernel, $parameterBag);
+    }
+
+    /**
+     * Configure the translator mock with the given translations
+     */
+    protected function configureTranslatorMock(array $translations): void
+    {
+        // Create a mock catalogue that will respond correctly to has() method
+        $mockCatalogue = $this->createMock(MessageCatalogueInterface::class);
+
+        // Configure the has method to return true for our test keys
+        $mockCatalogue->method('has')
+            ->willReturnCallback(function($id, $domain) use ($translations) {
+                return isset($translations[$domain][$id]);
+            });
+
+        // Configure the get method to return our translations
+        $mockCatalogue->method('get')
+            ->willReturnCallback(function($id, $domain) use ($translations) {
+                return $translations[$domain][$id] ?? $id;
+            });
+
+        // Configure the translator mock to return our catalogue
+        $this->symTranslator->method('getCatalogue')
+            ->willReturn($mockCatalogue);
+
+        // Configure the translator mock to return our test translations
+        $this->symTranslator->method('trans')
+            ->willReturnCallback(function($id, $parameters, $domain, $locale) use ($translations) {
+                $translation = $translations[$domain][$id] ?? $id;
+
+                foreach ($parameters as $key => $value) {
+                    $translation = str_replace($key, $value, $translation);
+                }
+
+                return $translation;
+            });
     }
 
     protected function tearDown(): void
--- tests/Unit/Translation/TranslationTest.php
+++ tests/Unit/Translation/TranslationTest.php
@@ -2,76 +2,80 @@
 
 namespace Wexample\SymfonyTranslations\Tests\Unit\Translation;
 
+use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Translation\Translator as SymfonyTranslator;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Translation\MessageCatalogue;
+use Symfony\Component\Translation\MessageCatalogueInterface;
 use Wexample\SymfonyTranslations\Tests\AbstractTranslationTest;
 use Wexample\SymfonyTranslations\Translation\Translator;
 
 class TranslationTest extends AbstractTranslationTest
 {
+    
+    protected function setUp(): void
+    {
+        parent::setUp();
+        
+        // Create mock objects
+        $this->symTranslator = $this->createMock(SymfonyTranslator::class);
+        $kernel = $this->createMock(KernelInterface::class);
+        $parameterBag = $this->createMock(ParameterBagInterface::class);
+
+        // Configure mocks
+        $kernel->method('getProjectDir')
+            ->willReturn(__DIR__);
+
+        $parameterBag->method('get')
+            ->with('translations_paths')
+            ->willReturn([__DIR__ . '/Resources/translations']);
+
+        // Configure the Symfony translator's getFallbackLocales method
+        $this->symTranslator->method('getFallbackLocales')
+            ->willReturn(['en']);
+
+        $this->symTranslator->method('getLocale')
+            ->willReturn('test');
+
+        // Create the translator instance
+        $this->translator = new Translator($this->symTranslator, $kernel, $parameterBag);
+    }
+
     /**
      * Test basic translation functionality
      */
     public function testBasicTranslation(): void
     {
-        /** @var Translator $translator */
-        $translator = $this->translator;
-
-        // Create a test catalogue with some translations
-        $catalogue = $this->createMock(\Symfony\Component\Translation\MessageCatalogueInterface::class);
+        $translations = [
+            'messages' => [
+                'test_key' => 'Test Value',
+                'simple_key' => 'Simple value',
+                'group.nested_key' => 'Nested translation value',
+                'welcome_message' => 'Hello %name%, welcome to our application!'
+            ]
+        ];
         
-        // Configure the has method to return true for our test keys
-        $catalogue->method('has')
-            ->willReturnCallback(function($id, $domain) {
-                return $domain === 'messages' && in_array($id, ['test_key', 'simple_key', 'group.nested_key', 'welcome_message']);
-            });
-
-        // Configure the get method to return our translations
-        $catalogue->method('get')
-            ->willReturnCallback(function($id, $domain) {
-                $translations = [
-                    'test_key' => 'Test Value',
-                    'simple_key' => 'Simple value',
-                    'group.nested_key' => 'Nested translation value',
-                    'welcome_message' => 'Hello %name%, welcome to our application!'
-                ];
-                
-                return $translations[$id] ?? $id;
-            });
-
-        // Configure the translator mock to return our catalogue
-        $translator->translator->method('getCatalogue')
-            ->willReturn($catalogue);
-
-        // Configure the translator mock to return our test translations
-        $translator->translator->method('trans')
-            ->willReturnCallback(function($id, $parameters, $domain, $locale) use ($catalogue) {
-                $translation = $catalogue->get($id, $domain);
-                
-                foreach ($parameters as $key => $value) {
-                    $translation = str_replace($key, $value, $translation);
-                }
-                
-                return $translation;
-            });
+        $this->configureTranslatorMock($translations);
 
         // Test basic translation with forceTranslate
         $this->assertEquals(
             'Test Value',
-            $translator->trans('test_key', [], 'messages', null, true),
+            $this->translator->trans('test_key', [], 'messages', null, true),
             'Basic translation should work correctly'
         );
 
         // Test simple key translation with forceTranslate
         $this->assertEquals(
             'Simple value',
-            $translator->trans('simple_key', [], 'messages', null, true),
+            $this->translator->trans('simple_key', [], 'messages', null, true),
             'Simple key translation should work correctly'
         );
 
         // Test with parameters and forceTranslate
         $this->assertEquals(
             'Hello John, welcome to our application!',
-            $translator->trans('welcome_message', ['%name%' => 'John'], 'messages', null, true),
+            $this->translator->trans('welcome_message', ['%name%' => 'John'], 'messages', null, true),
             'Translation with parameters should work correctly'
         );
     }
@@ -81,72 +85,38 @@ class TranslationTest extends AbstractTranslationTest
      */
     public function testDomainResolution(): void
     {
-        /** @var Translator $translator */
-        $translator = $this->translator;
-
-        // Create a test catalogue with domain-specific translations
-        $catalogue = new MessageCatalogue('test');
-        $catalogue->add(['page_title' => 'Welcome to our site'], 'app.pages.home');
-        $catalogue->add(['header' => 'Main Header'], 'app.components.header');
+        $translations = [
+            'app.pages.home' => [
+                'page_title' => 'Welcome to our site'
+            ],
+            'app.components.header' => [
+                'header' => 'Main Header'
+            ],
+            'TestBundle.pages.index' => [
+                'bundle_title' => 'Bundle Title'
+            ]
+        ];
         
-        // Add a bundle translation to test the assets handling
-        $catalogue->add(['bundle_title' => 'Bundle Title'], 'TestBundle.pages.index');
-        
-        // Create a mock catalogue that will respond correctly to has() method
-        $mockCatalogue = $this->createMock(\Symfony\Component\Translation\MessageCatalogueInterface::class);
-        
-        // Configure the has method to return true for our test keys
-        $mockCatalogue->method('has')
-            ->willReturnCallback(function($id, $domain) {
-                return ($domain === 'app.pages.home' && $id === 'page_title') ||
-                       ($domain === 'app.components.header' && $id === 'header') ||
-                       ($domain === 'TestBundle.pages.index' && $id === 'bundle_title');
-            });
-
-        // Configure the translator mock to return our mock catalogue
-        $translator->translator->method('getCatalogue')
-            ->willReturn($mockCatalogue);
-
-        // Configure the translator mock to return our test translations
-        $translator->translator->method('trans')
-            ->willReturnCallback(function($id, $parameters, $domain, $locale) {
-                $translations = [
-                    'app.pages.home' => [
-                        'page_title' => 'Welcome to our site'
-                    ],
-                    'app.components.header' => [
-                        'header' => 'Main Header'
-                    ],
-                    'TestBundle.pages.index' => [
-                        'bundle_title' => 'Bundle Title'
-                    ]
-                ];
-                
-                if (isset($translations[$domain][$id])) {
-                    return $translations[$domain][$id];
-                }
-                
-                return $id;
-            });
+        $this->configureTranslatorMock($translations);
         
         // Test domain resolution with explicit domain
         $this->assertEquals(
             'Welcome to our site',
-            $translator->trans('page_title', [], 'app.pages.home', null, true),
+            $this->translator->trans('page_title', [], 'app.pages.home', null, true),
             'Translation with explicit domain should work correctly'
         );
         
         // Test domain resolution with domain in the key
         $this->assertEquals(
             'Welcome to our site',
-            $translator->trans('app.pages.home::page_title', [], null, null, true),
+            $this->translator->trans('app.pages.home::page_title', [], null, null, true),
             'Translation with domain in the key should work correctly'
         );
         
         // Test bundle domain resolution
         $this->assertEquals(
             'Bundle Title',
-            $translator->trans('bundle_title', [], 'TestBundle.pages.index', null, true),
+            $this->translator->trans('bundle_title', [], 'TestBundle.pages.index', null, true),
             'Translation with bundle domain should work correctly'
         );
     }
@@ -156,59 +126,34 @@ class TranslationTest extends AbstractTranslationTest
      */
     public function testDomainStack(): void
     {
-        /** @var Translator $translator */
-        $translator = $this->translator;
+        $translations = [
+            'app.pages.home' => [
+                'title' => 'Home Page Title'
+            ],
+            'app.pages.about' => [
+                'title' => 'About Page Title'
+            ]
+        ];
         
-        // Create a mock catalogue that will respond correctly to has() method
-        $mockCatalogue = $this->createMock(\Symfony\Component\Translation\MessageCatalogueInterface::class);
-        
-        // Configure the has method to return true for our test keys
-        $mockCatalogue->method('has')
-            ->willReturnCallback(function($id, $domain) {
-                return ($domain === 'app.pages.home' && $id === 'title') ||
-                       ($domain === 'app.pages.about' && $id === 'title');
-            });
-
-        // Configure the translator mock to return our mock catalogue
-        $translator->translator->method('getCatalogue')
-            ->willReturn($mockCatalogue);
-
-        // Configure the translator mock to return our test translations
-        $translator->translator->method('trans')
-            ->willReturnCallback(function($id, $parameters, $domain, $locale) {
-                $translations = [
-                    'app.pages.home' => [
-                        'title' => 'Home Page Title'
-                    ],
-                    'app.pages.about' => [
-                        'title' => 'About Page Title'
-                    ]
-                ];
-                
-                if (isset($translations[$domain][$id])) {
-                    return $translations[$domain][$id];
-                }
-                
-                return $id;
-            });
+        $this->configureTranslatorMock($translations);
         
         // Set up domain stack
-        $translator->setDomain('page', 'app.pages.home');
+        $this->translator->setDomain('page', 'app.pages.home');
         
         // Test domain resolution with domain alias
         $this->assertEquals(
             'Home Page Title',
-            $translator->trans('@page::title', [], null, null, true),
+            $this->translator->trans('@page::title', [], null, null, true),
             'Translation with domain alias should work correctly'
         );
         
         // Change the domain in the stack
-        $translator->setDomain('page', 'app.pages.about');
+        $this->translator->setDomain('page', 'app.pages.about');
         
         // Test domain resolution with updated domain alias
         $this->assertEquals(
             'About Page Title',
-            $translator->trans('@page::title', [], null, null, true),
+            $this->translator->trans('@page::title', [], null, null, true),
             'Translation with updated domain alias should work correctly'
         );
     }
