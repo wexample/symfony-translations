6fd5546057bc474b9166fdae07b47c095ec68118
weeger <romain.weeger@gmail.com>
2026-07-28 11:27:53 +0200

    Cleanup

--- src/Repository/LocaleRepository.php
+++ src/Repository/LocaleRepository.php
@@ -4,25 +4,25 @@ namespace Wexample\SymfonyTranslations\Repository;
 
 use Doctrine\Persistence\ManagerRegistry;
 use Wexample\SymfonyHelpers\Repository\AbstractRepository;
-use Wexample\SymfonyTranslations\Entity\Locale;
+use Wexample\SymfonyTranslations\Entity\AbstractLocale;
 
 /**
- * @method Locale|null find($id, $lockMode = null, $lockVersion = null)
- * @method Locale|null findOneBy(array $criteria, array $orderBy = null)
- * @method Locale|null findOneByCode(string $code)
- * @method Locale[]    findAll()
- * @method Locale[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method AbstractLocale|null find($id, $lockMode = null, $lockVersion = null)
+ * @method AbstractLocale|null findOneBy(array $criteria, array $orderBy = null)
+ * @method AbstractLocale|null findOneByCode(string $code)
+ * @method AbstractLocale[]    findAll()
+ * @method AbstractLocale[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 abstract class LocaleRepository extends AbstractRepository
 {
     public function __construct(
         ManagerRegistry $registry,
-        $entityClass = Locale::class
+        $entityClass = AbstractLocale::class
     ) {
         parent::__construct($registry, $entityClass);
     }
 
-    public function findDefault(): ?Locale
+    public function findDefault(): ?AbstractLocale
     {
         return $this->findOneBy(['isDefault' => true]);
     }
--- src/Repository/TranslationRepository.php
+++ src/Repository/TranslationRepository.php
@@ -4,25 +4,25 @@ namespace Wexample\SymfonyTranslations\Repository;
 
 use Doctrine\Persistence\ManagerRegistry;
 use Wexample\SymfonyHelpers\Repository\AbstractRepository;
-use Wexample\SymfonyTranslations\Entity\Locale;
-use Wexample\SymfonyTranslations\Entity\Translation;
+use Wexample\SymfonyTranslations\Entity\AbstractLocale;
+use Wexample\SymfonyTranslations\Entity\AbstractTranslation;
 
 /**
- * @method Translation|null find($id, $lockMode = null, $lockVersion = null)
- * @method Translation|null findOneBy(array $criteria, array $orderBy = null)
- * @method Translation[]    findAll()
- * @method Translation[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method AbstractTranslation|null find($id, $lockMode = null, $lockVersion = null)
+ * @method AbstractTranslation|null findOneBy(array $criteria, array $orderBy = null)
+ * @method AbstractTranslation[]    findAll()
+ * @method AbstractTranslation[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 abstract class TranslationRepository extends AbstractRepository
 {
     public function __construct(
         ManagerRegistry $registry,
-        $entityClass = Translation::class
+        $entityClass = AbstractTranslation::class
     ) {
         parent::__construct($registry, $entityClass);
     }
 
-    public function findByLocaleAndKey(Locale $locale, string $key): ?Translation
+    public function findByLocaleAndKey(AbstractLocale $locale, string $key): ?AbstractTranslation
     {
         return $this->findOneBy([
             'locale' => $locale,
@@ -31,9 +31,9 @@ abstract class TranslationRepository extends AbstractRepository
     }
 
     /**
-     * @return Translation[]
+     * @return AbstractTranslation[]
      */
-    public function findAllByLocale(Locale $locale): array
+    public function findAllByLocale(AbstractLocale $locale): array
     {
         return $this->findBy(['locale' => $locale]);
     }
